0a1f81063bfe21bd4d5cf37c9d26dc5230285244
krotov <krotov.s.vl@gmail.com>
2017-07-27 13:54:32 +0300

    @#@!##@#@#!!!!

--- src/main/java/logics/DBconnect.java
+++ src/main/java/logics/DBconnect.java
@@ -13,19 +13,25 @@ public class DBconnect {
     public static void main(String[] args) {
         {
             try {
-                Driver driver = new FabricMySQLDriver();
-            DriverManager.registerDriver(driver);
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/psdatabase", "root", "root");
 //                Driver driver = new FabricMySQLDriver();
-//                DriverManager.registerDriver(driver);
-//                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/psdatabase", "root", "root");
-            } catch (SQLException | ClassNotFoundException e) {
+//            DriverManager.registerDriver(driver);
+//            Class.forName("com.mysql.jdbc.Driver");
+//            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/psdatabase", "root", "root");
+                Driver driver = new FabricMySQLDriver();
+                DriverManager.registerDriver(driver);
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/psdatabase", "root", "root");
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
+//                catch (ClassNotFoundException e) {
+//                e.printStackTrace();
+//            }
         }
         System.out.println("Test");
         System.out.println(connection);
+        DAOmysql dao = new DAOmysql();
+        dao.testDB(connection);
+        System.out.println("!!!!check: " + dao.checkClientExistence("Sergey", "Krotov"));
 
     }
 
